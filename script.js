886099609121059a72d17f99895c83f3f2955b18
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-23 03:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr){
+	arr.sort((a, b) => a - b);
+    let totalCost = 0;
+    while (arr.length > 1) {
+        const shortest1 = arr.shift();
+        const shortest2 = arr.shift();
+        const combinedLength = shortest1 + shortest2;
+        totalCost += combinedLength;
+        let insertIndex = 0;
+        while (insertIndex < arr.length && arr[insertIndex] < combinedLength) {
+            insertIndex++;
+        }
+        arr.splice(insertIndex, 0, combinedLength);
+    }
 
+    return totalCost;
+}
 module.exports=mincost;
